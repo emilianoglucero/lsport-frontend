c2e62fe0153c04e807fe3138d138ca248fd9f131
Emiliano Lucero <elucero@lenguajesport.com>
2019-01-17 18:06:15 -0300

    Remove the now ignored directory platforms, plugins, hooks, ul_web_hooks

--- platforms/ios/www/js/pages/page-unsupported.js
+++
@@ -1,9 +0,0 @@
-myApp.onPageInit('unsupported', function (page)
-{
-	$('#pFirstUnsupported').text(lblMessage1Unsupported);
-});
-
-myApp.onPageBeforeAnimation('unsupported', function (page)
-{
-	myApp.params.swipePanel = false;
-});
