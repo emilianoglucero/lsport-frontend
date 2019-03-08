e9e5c0e94000c7b6e5e1199183a375835b90cd5c
Emiliano Lucero <elucero@lenguajesport.com>
2019-03-08 19:15:34 -0300

    table position bug fixed, template-club.css improve

--- www/js/pages/tournaments/page-positionstable.js
+++ www/js/pages/tournaments/page-positionstable.js
@@ -97,7 +97,7 @@ console.log(item);
 	var strBuilderListCards = [];
 	//$.each(positionTables, function(n, table) {
 	//console.log(table);
-	strBuilderListCards.push('<div class="card tournament-matches"> <a onclick="loadPositionsTableDetails('+item.id+')" href="#">');
+	strBuilderListCards.push('<div class="card tournament-matches">');
                           strBuilderListCards.push('<div id="tournament-matches-header" class="card-header no-border">');
 
                           strBuilderListCards.push('<div class="tournament-header-titulo">'+item.titulo+'</div>');
@@ -198,115 +198,6 @@ console.log(item);
 
                     strBuilderListCards.push('</div></div>');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		/*strBuilderListCards.push('<div class="card card-table-tournaments">');
-		strBuilderListCards.push('<div class="card-header card-header-center card-header-positionstable">'+positionTables.titulo+'</div>');
-		strBuilderListCards.push('<div class="card-header card-header-center">'+positionTables.titulo+'</div>');
-		strBuilderListCards.push('<div class="card-content">');
-		strBuilderListCards.push('<div class="card-content-inner">');
-		strBuilderListCards.push('<div class="list-block">');
-		strBuilderListCards.push('<div style="overflow-x:auto;">');
-		strBuilderListCards.push('<table class="table-tournaments table-positionstable">');
-		strBuilderListCards.push('<tr class="tr-header">');
-			$.each(positionTables.tablaGeneral.cabecera, function(i, item) {
-            //console.log(item.nombreCorto);
-            //console.log(item.columna);
-                if (item.columna == 'eq'){
-                    if(item.columna != "" && item.columna != undefined){
-                        strBuilderListCards.push('<th class="th-40-tournaments">'+item.nombreCorto+'</th>');
-                    }
-                } else if (item.columna == 'pt'){
-                    if(item.columna != "" && item.columna != undefined){
-                        strBuilderListCards.push('<th class="th-10-tournaments">'+item.nombreCorto+'</th>');
-                    }
-                } else if (item.columna == 'pj'){
-                    if(item.columna != "" && item.columna != undefined){
-                        strBuilderListCards.push('<th class="th-10-tournaments">'+item.nombreCorto+'</th>');
-                    }
-                } else if (item.columna == 'pe'){
-                    if(item.columna != "" && item.columna != undefined){
-                        strBuilderListCards.push('<th class="th-8-tournaments">'+item.nombreCorto+'</th>');
-                    }
-                } else if (item.columna == 'pp'){
-                    if(item.columna != "" && item.columna != undefined){
-                        strBuilderListCards.push('<th class="th-8-tournaments">'+item.nombreCorto+'</th>');
-                    }
-                } else if (item.columna == 'tf'){
-                    if(item.columna != "" && item.columna != undefined){
-                        strBuilderListCards.push('<th class="th-8-tournaments">'+item.nombreCorto+'</th>');
-                    }
-                } else if (item.columna == 'tc'){
-                    if(item.columna != "" && item.columna != undefined){
-                        strBuilderListCards.push('<th class="th-8-tournaments">'+item.nombreCorto+'</th>');
-                    }
-                } else if (item.columna == 'td'){
-                    if(item.columna != "" && item.columna != undefined){
-                        strBuilderListCards.push('<th class="th-8-tournaments">'+item.nombreCorto+'</th>');
-                    }
-                } else if (item.columna == 'pt'){
-                    if(item.columna != "" && item.columna != undefined){
-                        strBuilderListCards.push('<th class="th-8-tournaments">'+item.nombreCorto+'</th>');
-                    }
-                }
-
-
-                //}
-            });
-		strBuilderListCards.push('</tr>');
-
-		$.each(positionTables.tablaGeneral.cuerpo, function(i, item) {
-			strBuilderListCards.push('<tr>');
-			var pos = i+1;
-			if(item.eq.nombre !== "" && item.eq.nombre !== undefined){
-                strBuilderListCards.push('<td class="td-40-tournaments"><span class="td-span-team-pos">'+pos+'</span><span class="td-span-team-name">'+item.eq.nombre+'</span></th>');
-            }
-			if(item.pt !== "" && item.pt !== undefined){
-                strBuilderListCards.push('<td class="td-10-tournaments">'+item.pt+'</th>');
-            }
-			if(item.pj !== "" && item.pj !== undefined){
-                strBuilderListCards.push('<td class="td-10-tournaments">'+item.pj+'</th>');
-            }
-			if(item.pe !== "" && item.pe !== undefined){
-                strBuilderListCards.push('<td class="td-8-tournaments">'+item.pe+'</th>');
-            }
-			if(item.pp !== "" && item.pp !== undefined){
-                strBuilderListCards.push('<td class="td-8-tournaments">'+item.pp+'</th>');
-            }
-			if(item.tf !== "" && item.tf !== undefined){
-                strBuilderListCards.push('<td class="td-8-tournaments">'+item.tf+'</th>');
-            }
-			if(item.tc !== "" && item.tc !== undefined){
-                strBuilderListCards.push('<td class="td-8-tournaments">'+item.tc+'</th>');
-            }
-			strBuilderListCards.push('</tr>');
-		});
-			
-		strBuilderListCards.push('</table>');
-		strBuilderListCards.push('</div>');
-		/*if(positionTables.tableNotes != "" || table.tableNotes != undefined){
-			strBuilderListCards.push('<div class="description-table-tournament">'+table.tableNotes+'</div>');
-		}*/ /*
-		strBuilderListCards.push('</div>');
-		strBuilderListCards.push('</div>');
-		strBuilderListCards.push('</div>');
-		strBuilderListCards.push('</div>');
-	//});*/
 	
 	$('#positionstable-list').append(strBuilderListCards.join(""));
 	mainView.router.load({pageName: 'positionstable'});
