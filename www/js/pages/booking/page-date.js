415cbadda309ca37ed8602fd336fbe4bea613217
Emiliano Lucero <elucero@lenguajesport.com>
2020-01-14 12:53:48 -0300

    working on date and time picker

--- www/js/pages/booking/page-date.js
+++ www/js/pages/booking/page-date.js
@@ -3,23 +3,49 @@ myApp.onPageInit('date', function (page)
     console.log('date page init');
 });
 
-/*var today = new Date();
-var yesterday = new Date().setDate(today.getDate() - 1);
 
-var monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-var dayNames = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
- 
-var calendarDisabled = myApp.calendar({
-    input: '#calendar-date-booking',
-    dateFormat: 'M dd yyyy',
-    disabled: {
-      from: new Date(1900, 1, 1),
-      to: yesterday
-    }
-});*/
- 
+  
+$$(document).on('DOMContentLoaded', function() {
 
-var calendarDefault = myApp.calendar({
-  input: '#calendar-default',
-});     
+  var today = new Date();
+  var yesterday = new Date().setDate(today.getDate() - 1);
+  
+  var monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+  var dayNames = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+   
+  var calendarDisabled = myApp.calendar({
+      input: '#calendar-date-booking',
+      dateFormat: 'D d MM yyyy',
+      disabled: {
+        from: new Date(1900, 1, 1),
+        to: yesterday
+      }
+  });
+
+
+ 
+  var pickerDescribe = myApp.picker({
+    input: '#picker-describe',
+    rotateEffect: true,
+    cols: [
+        {
+            textAlign: 'left',
+            values: ('00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23').split(' ')
+        },
+        // Divider
+        {
+            divider: true,
+            content: ':'
+        },
+        {
+            values: ('00 30').split(' ')
+        },
+    ]
+  });                                
+
+
+
+
+});
+  
 
--- www/js/pages/page-home.js
+++ www/js/pages/page-home.js
@@ -8,24 +8,3 @@ function serviceRerservation(id){
 }
 
 
-var calendarDateFormat = myApp.calendar({
-    input: '#calendar-date-format',
-    dateFormat: 'DD, MM dd, yyyy'
-});
-
-var calendarDefault = myApp.calendar({
-    input: '#calendar-default',
-});     
-
-$$('.popup-about').on('popup:opened', function () {
-    console.log('About Popup opened')
-  });
-  $$('.popup-about').on('popup:close', function () {
-    console.log('About Popup is closing')
-  });
-  $$('.popup-services').on('popup:open', function () {
-    console.log('Services Popup is opening')
-  });
-  $$('.popup-services').on('popup:closed', function () {
-    console.log('Services Popup is closed')
-  });
